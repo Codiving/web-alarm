3ccf0d160caefe96ca10ee6973f352d84202fd20
Codiving <codivingmoon@gmail.com>
2025-06-01 21:50:25 +0900

    storage set utils 함수 구현

--- src/popup/storage.ts
+++ src/popup/storage.ts
@@ -1,7 +1,20 @@
-export const getFromStorage = <T = unknown>(key: string): Promise<T | null> => {
+type Key = "lastSelectedDays" | "alarms" | "closedAlarms";
+
+export const getFromStorage = <T = unknown>(key: Key): Promise<T | null> => {
   return new Promise(resolve => {
     chrome.storage.local.get(key, result => {
       resolve(result[key] ?? null);
+    });
+  });
+};
+
+export const setToStorage = <T = unknown>(
+  key: Key,
+  value: T
+): Promise<void> => {
+  return new Promise(resolve => {
+    chrome.storage.local.set({ [key]: value }, () => {
+      resolve();
     });
   });
 };
