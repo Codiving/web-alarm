d9a6166afea6283510006235e115b4a1d57b7a7d
Codiving <codivingmoon@gmail.com>
2025-06-02 18:58:30 +0900

    설정값들 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -12,18 +12,17 @@ export default defineConfig({
         popup: resolve(__dirname, "popup.html"),
         background: resolve(__dirname, "src/background.js"),
         content: resolve(__dirname, "src/content/index.js"),
-        popupScript: resolve(__dirname, "src/popup/index.js"),
         popupCss: resolve(__dirname, "src/popup/popup.css"),
-        contentCss: resolve(__dirname, "src/content/content.css")
+        contentCss: resolve(__dirname, "src/content/content.css"),
       },
       output: {
         inlineDynamicImports: false,
         entryFileNames: "[name].js",
-        assetFileNames: "[name].[ext]"
-      }
+        assetFileNames: "[name].[ext]",
+      },
     },
     outDir: "dist",
-    emptyOutDir: true
+    emptyOutDir: true,
   },
-  plugins: [tailwindcss(), react()]
+  plugins: [tailwindcss(), react()],
 });
