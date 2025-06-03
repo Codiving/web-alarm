5bff849315d787ff72d8f9c8e46417f203e7f4ea
Codiving <codivingmoon@gmail.com>
2025-06-03 11:34:36 +0900

    로케일 업데이트

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,4 +1,29 @@
-import { LocaleKey } from "../type/day";
+type LocaleKey =
+  | "extName"
+  | "extDescription"
+  | "am"
+  | "pm"
+  | "sun"
+  | "mon"
+  | "tue"
+  | "wed"
+  | "thu"
+  | "fri"
+  | "sat"
+  | "oneTimeAlarm"
+  | "oneTimeAlarmTooltip"
+  | "activeDays"
+  | "activeDate"
+  | "editAlarm"
+  | "addAlarm"
+  | "day"
+  | "month"
+  | "year"
+  | "save"
+  | "memo"
+  | "invalidDate"
+  | "pastDateNotAllowed"
+  | "pastTimeNotAllowed";
 
 export function t(key: LocaleKey): string {
   return chrome.i18n.getMessage(key) || key;
